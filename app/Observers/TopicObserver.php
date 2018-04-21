1c036ad4f43111bc4ec7dafaa02bee18ce9a6dbc
vagrant <vagrant@homestead>
2018-04-21 10:31:05 +0000

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,22 +3,23 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
+
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 //模型观察器，对模型从创建到结束进行监听，触发以上方法是触发事件
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
+//    public function creating(Topic $topic)
+//    {
+//        //
+//    }
+//
+//    public function updating(Topic $topic)
+//    {
+//        //
+//    }
     //保存时，截取excerpt作为摘录，用于SEO搜索引擎优化。make_excerpt为辅助方法，在bootstrap/helpers.php
     public function saving(Topic $topic)
     {
@@ -26,9 +27,15 @@ class TopicObserver
 
         $topic->excerpt = make_excerpt($topic->body);        // 生成话题摘录
 
+
+    }
+
+    public function saved(Topic $topic){
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
